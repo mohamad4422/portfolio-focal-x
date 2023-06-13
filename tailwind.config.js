6b977d2fe43a3e0f45d9e95c101421138d81d39a
MohamadAlgazli <brawjym@gmail.com>
2023-06-13 17:26:33 +0300

    adding basic styles and imgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}",],
   theme: {
-    extend: {
-      colors: {
-        accent: "#F9CC11",
-        primary: "#FFFFFF",
-      }
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
     },
+    fontWeight: {
+      light: 300,
+      base: 400,
+      semibold: 600,
+      bold: 700,
+    },
+    colors: {
+      accent: {
+        DEFAULT: "#F9CC11",
+        content: "#030303"
+      },
+      primary: "#FFFFFF",
+      neutral: {
+        300: "#1D1C1C",
+        200: "#696969",
+        100: "#D9D9D9"
+      },
+      background: "#000000",
+    },
+    borderRadius: {
+      DEFAULT: "10px",
+      lg: "20px",
+    }
   },
   plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     cssCodeSplit: true,
-    assetsDir: "css",
     rollupOptions: {
       input: {
         index: "src/index.html",
