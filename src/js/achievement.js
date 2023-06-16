fe6758c97608dabb65c7d2c4a58291459b79686b
Ali Darwish <alidarwish502@gmail.com>
2023-06-16 16:19:01 +0300

    Built the achievement section with swapper.js library

---
+++ src/js/achievement.js
@@ -0,0 +1,10 @@
+import Swiper from "swiper";
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
